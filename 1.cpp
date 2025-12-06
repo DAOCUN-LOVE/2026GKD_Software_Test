af0990dd16528854e0f8b91b68c9eab8f72a981d
DAOCUN-LOVE <2318037133@qq.com>
2025-12-06 15:21:54 +0800

    第二版
    第二版，完成了所有子类的编写和测试

--- 1.cpp
+++ 1.cpp
@@ -39,7 +39,7 @@ public:
 	using sensortaskbase::sensortaskbase;//看看没有override会怎样
 	void run()  override{
 		running = true;
-		cout << "[Filter" << key << "开始]"<<endl;
+		cout << "[Filter-" << key << "开始]"<<endl;
 		while (running) {
 			//检查是否有任务
 			if (p_in == nullptr || p_out == nullptr) {
@@ -56,7 +56,7 @@ public:
 			std::this_thread::sleep_for(chrono::milliseconds(1));
 
 		}
-		cout << "[Filter" << key << "结束]"<<endl;
+		cout << "[Filter-" << key << "结束]"<<endl;
 	}
 	void stop() override {
 		running = false;
@@ -68,7 +68,82 @@ public:
 		
 	}
 };
+class TaskGain :public sensortaskbase {
+private:
+	atomic<int> k;
+public:
+	using sensortaskbase::sensortaskbase;
+	TaskGain(int task_key) :sensortaskbase(task_key), k(1) {};
+	void run()override {
+		running = true;
+		cout << "[Gain-" << key << "开始]" << "放大倍数k==" << k << endl;
+		while (running) {
+			if (p_in == nullptr || p_out == nullptr) {
+				std::this_thread::sleep_for(std::chrono::milliseconds(1));
+				continue;
+			}
+
+			int input_val = p_in->load(memory_order_relaxed);
+			if (input_val != 0) {
+				int output_val = input_val * k;
+				p_out->store(output_val, memory_order_relaxed);//int current_k = k.load(memory_order_relaxed);  // 读取当前k值 int output_val = input_val * current_k;
+				p_in->store(0, memory_order_relaxed);
+				cout << "writer gain-" << ":" << output_val << endl;
+			}
+			std::this_thread::sleep_for(chrono::milliseconds(1));
+		}
+		cout << "[Gain-" << key << "结束]" << endl;
+	}
+	void stop() override {
+		running = false;
+	}
+	void callback(int msg) override {
+		k.store(msg, memory_order_relaxed);
+		if (p_in != nullptr) {
+			p_in->store(1, memory_order_relaxed);
+		}
+		cout << "[Gain-" << key << "] 回调注入值: " << msg << endl;  // 调试日志（可选）
+	}
+};
+class TaskDelayBuffer :public sensortaskbase {
+public:
+	using sensortaskbase::sensortaskbase;
+	void run()override {
+		running = true;
+		cout << "[DelayBuffer-" << key << "开始]" << endl;
+		while (running) {
+			if (p_in == nullptr || p_out == nullptr) {
+				std::this_thread::sleep_for(std::chrono::milliseconds(1));
+				continue;
+			}
+			int input_val = p_in->load(memory_order_relaxed);
+			if (input_val != 0) {
+				int t = input_val;
+				p_in->store(0, memory_order_relaxed);
+				p_out->store(t, memory_order_relaxed);
+				cout << "writer delaybuffer-" << ":" << t << endl;
+				this_thread::sleep_for(chrono::milliseconds(1));//模拟延时
+				p_out->store(t + 1, memory_order_relaxed);
+				cout << "writer delaybuffer-" << ":" << t + 1 << endl;
+			}
+			this_thread::sleep_for(chrono::milliseconds(1));
+		}
+		cout << "[DelayBuffer-" << key << "] 结束" << endl;
+	}
+	void stop() override {
+		running = false;
+	}
+	void callback(int msg) override {
+		if (p_in != nullptr) {
+			p_in->store(msg, memory_order_relaxed);
+		}
+		cout << "[DelayBuffer-" << key << "] 回调注入值: " << msg << endl;  // 调试日志（可选）
+	}
+};
+
+
 int main() {
+	//第一个程序：滤波器任务的test
 	atomic<int>input_buf = 0;
 	atomic<int>output_buf = 0;
 	TaskFilter filter_task(1);
@@ -78,11 +153,43 @@ int main() {
 	filter_task.callback(10);
 	this_thread::sleep_for(chrono::milliseconds(100));
 	filter_task.stop();
-	// 等待线程退出
-	if (task_thread.joinable()) {
-		task_thread.join();}
+	if (task_thread.joinable()) {// 等待线程退出
+		task_thread.join();
+	}
+	
 
 	cout << "最终输出结果为：" << output_buf.load() << endl;
 	cout << filter_task.get_key()<<endl;//查看任务序数
+
+	//第二个程序：增益任务的test
+	TaskGain gain_task(2);
+	gain_task.setpointer(&input_buf, &output_buf);
+	thread gain_thread(&TaskGain::run, &gain_task);
+	this_thread::sleep_for(chrono::milliseconds(100));
+	gain_task.callback(5); //设置增益k=5
+	this_thread::sleep_for(chrono::milliseconds(100));
+	gain_task.callback(3); //注入数据3
+	this_thread::sleep_for(chrono::milliseconds(100));
+	gain_task.stop();
+	// 等待线程退出
+	if (gain_thread.joinable()) {
+		gain_thread.join();
+	}
+
+
+	//第三个程序：延时缓冲任务的test
+	TaskDelayBuffer delay_task(3);
+	delay_task.setpointer(&input_buf, &output_buf);
+	thread delay_thread(&TaskDelayBuffer::run, &delay_task);
+	this_thread::sleep_for(chrono::milliseconds(100));
+	delay_task.callback(7); //注入数据7
+	this_thread::sleep_for(chrono::milliseconds(200));
+	delay_task.stop();
+	// 等待线程退出
+	if (delay_thread.joinable()) {
+		delay_thread.join();
+	}
+
+	cout << "最终输出结果为：" << output_buf.load() << endl;	
 	return 0;
 }
